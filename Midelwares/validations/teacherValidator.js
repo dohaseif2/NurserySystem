a0696c08a77d6f1fcf6c1c1f2dbc1c127beec093
dohaseif2 <dohaseif2@gmail.com>
2024-03-31 19:21:20 +0200

    make email unique

--- Midelwares/validations/teacherValidator.js
+++ Midelwares/validations/teacherValidator.js
@@ -1,8 +1,17 @@
 const { body, param, query } = require("express-validator");
+
+const Teacher = require('./../../Models/teacherSchema'); 
+
 exports.insertValidator = [
   body("fullName").isAlpha().withMessage("teacher username should be string").isLength({min : 4 }).withMessage("UserName should be > 4"),
   body("password").isLength({min:6}).withMessage('password should be >6'),
-  body("email").isEmail().withMessage("Invalid email address"),
+  body("email").isEmail().withMessage("Invalid email address").custom(async (value) => {
+    const user = await Teacher.findOne({ email: value });
+    if (user) {
+      throw new Error('Email already exists');
+    }
+    return true;
+  }),
   body("supervisor").isBoolean().withMessage("Supervisor should be a boolean value (true/false)"),
   body("role").isIn(["admin", "teacher"]).withMessage("Role should be either (admin/teacher)")
 ];
--- Models/teacherSchema.js
+++ Models/teacherSchema.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const teacherSchema = new mongoose.Schema({
     fullName: { type: String },
     password: { type: String },
-    email: { type: String },
+    email: { type: String, unique: true },
     image: { type: String },
     supervisor: { type: Boolean },
     role: { type: String }
